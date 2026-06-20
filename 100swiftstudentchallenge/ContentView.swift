17f1e99a113c83dfa46da455a5c13037b67d9798
Gleen Ryan <tjiagleenryan@gmail.com>
2026-06-20 16:29:32 +0800

    Initial Commit

---
+++ 100swiftstudentchallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  100swiftstudentchallenge
+//
+//  Created by Gleenryan on 20/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100swiftstudentchallenge/_00swiftstudentchallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00swiftstudentchallengeApp.swift
+//  100swiftstudentchallenge
+//
+//  Created by Gleenryan on 20/06/26.
+//
+
+import SwiftUI
+
+@main
+struct _00swiftstudentchallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
