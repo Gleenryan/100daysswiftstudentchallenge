3f5cb51fcdebb18f9908bb9186b73c134ae8abc6
Gleen Ryan <tjiagleenryan@gmail.com>
2026-06-28 12:39:39 +0800

    day21

---
+++ 100swiftstudentchallenge/day21.swift
@@ -0,0 +1,104 @@
+//
+//  day21.swift
+//  100swiftstudentchallenge
+//
+//  Created by Gleenryan on 28/06/26.
+//
+
+
+import SwiftUI
+
+struct day21: View {
+    
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled() // bakal terus ke shuffle
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State var correctAnswer = Int.random(in: 0...2)
+    var body: some View {
+        ZStack{
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Text("Guess The Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                VStack{
+                    VStack{
+                        Text("Tap the flag of")
+                            .foregroundStyle(Color.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                
+                            .font(.largeTitle.weight(.bold))
+                        
+                    }
+                    
+                    
+                    
+                    ForEach(0..<3){number in
+                        Button{
+                            flagTapped(number)
+                        }label: {
+                            Image(self.countries[number])
+                                .clipShape(.circle)
+                                .shadow(radius: 5)
+                        }
+                        
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity)
+                    .background(.regularMaterial)
+                    .padding(10)
+                
+                Spacer()
+                Spacer()
+                
+                Text("Your Score is:")
+                    .foregroundStyle(.white)
+                    .font(.largeTitle.weight(.heavy))
+            
+                
+            }
+            
+            
+            
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
+        
+        
+        
+        
+    }
+    
+    func flagTapped(_ number: Int){//funch harus diluar body
+        if number == correctAnswer{
+            scoreTitle = "Correct"
+        }
+        else{
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+}
+
+#Preview {
+    day21()
+}
