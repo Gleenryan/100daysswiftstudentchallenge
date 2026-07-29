406f6ebabbc5b493296f18385de78a93b84b8cf9
Gleen Ryan <tjiagleenryan@gmail.com>
2026-07-29 14:52:49 +0800

    Create day34.swift

---
+++ 100swiftstudentchallenge/day34.swift
@@ -0,0 +1,9 @@
+//
+//  day34.swift
+//  100swiftstudentchallenge
+//
+//  Created by Gleenryan on 29/07/26.
+//
+//skipped beczuse i don't want to focus on animation, I already of what it can do and how powerful it is, skipping to the nex tone.
+
+//i've done the test tho and score 10 / 12
