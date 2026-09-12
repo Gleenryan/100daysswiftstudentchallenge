6578b009305de90a4eaf6e90235a659235c6abd2
Gleen Ryan <tjiagleenryan@gmail.com>
2026-09-12 16:01:08 +0800

    89

--- 100swiftstudentchallenge/project17/CardView.swift
+++ 100swiftstudentchallenge/project17/CardView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    
     
     let card: Card
     @State var isShowingAnswer = false
@@ -19,7 +21,19 @@ struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
-                .fill(.white)
+                .fill(
+                    accessibilityDifferentiateWithoutColor
+                        ? .white
+                        : .white
+                            .opacity(1 - Double(abs(offset.width / 50)))
+
+                )
+                .background(
+                    accessibilityDifferentiateWithoutColor
+                        ? nil
+                        : RoundedRectangle(cornerRadius: 25)
+                            .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
 
             VStack {
